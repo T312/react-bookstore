42e9a3e336f1a3e4f4f0ae9a301847c40acb5f03
T312 <nl1903067@gmail.com>
2022-10-03 21:29:06 +0700

    build models

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/connectMongoDB.js";
 
+dotenv.config();
+connectDB();
 const app = express();
-const PORT = process.env.PORT || 5000;
+app.use(express.json());
+
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ backend/models/UserModel.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    phone: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    postalCode: {
+      type: String,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
