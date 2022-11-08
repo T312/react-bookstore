e6cc213fa914d390dab18b1abc4bc84575f2f9c5
T312 <nl1903067@gmail.com>
2022-11-08 07:40:59 +0700

    change_productAPI

--- backend/controller/ProductController.js
+++ backend/controller/ProductController.js
@@ -81,7 +81,9 @@ const getProductById = asyncHandler(async (req, res) => {
   }
   const product = await Product.findById(req.params.id).populate("category");
   if (product) {
-    res.json(product);
+    res.json({
+      product: product,
+    });
   } else {
     res.status(404);
     throw new Error("Product not Found");
