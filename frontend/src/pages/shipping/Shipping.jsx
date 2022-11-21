d68668b0769b230f25d01a446e24e6e34bf1d419
Trung-Hieu Lam <hieulam0918@gmail.com>
2022-11-22 01:40:48 +0700

    feat[checkout-status]: release shipping page

--- frontend/src/pages/shipping/Shipping.jsx
+++ frontend/src/pages/shipping/Shipping.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import AddressCard from "../../components/address-card/AddressCard";
 import CheckoutStatus from "../../components/checkout-status/CheckoutStatus";
+import Grid from "../../components/grid/Grid";
 import Section, {
   SectionTitle,
   SectionBody,
@@ -12,11 +13,12 @@ const Shipping = () => {
     <div className='container'>
       <Section>
         <SectionTitle>---</SectionTitle>
-        <SectionTitle>Thanh toán</SectionTitle>
+        <SectionTitle>Thủ tục thanh toán</SectionTitle>
         <SectionBody>
           <CheckoutStatus />
         </SectionBody>
       </Section>
+
       <div className='shipping'>
         <AddressCard />
       </div>
