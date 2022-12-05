ace389de9dbcafe72029e9b49a66597a25894f80
T312 <nl1903067@gmail.com>
2022-12-05 23:26:05 +0700

    feat[userCreate]

--- dashboard/src/components/sidebar.js
+++ dashboard/src/components/sidebar.js
@@ -84,16 +84,6 @@ const Sidebar = () => {
                 <span className="text">Người dùng</span>
               </NavLink>
             </li>
-            <li className="menu-item">
-              <NavLink
-                activeClassName="active"
-                className="menu-link disabled"
-                to="/transaction"
-              >
-                <i className="icon fas fa-usd-circle"></i>
-                <span className="text">Giao dịch</span>
-              </NavLink>
-            </li>
           </ul>
           <br />
           <br />
